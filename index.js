1b06a173face97920a6a43ab8a8d6a4a57aaf209
MsDiala <diala-is-diala@hotmail.com>
2023-10-26 19:01:37 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Example data for testing
+  const drivers = [
+    "Diala",
+    "Aya",
+    "Yazan",
+    "Diala",
+    "Duaa",
+  ];
+  
